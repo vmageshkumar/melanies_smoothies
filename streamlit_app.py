587ce82ed0866ece708c487734d109c1bb10144e
Mahesh Kumar <69344132+vmageshkumar@users.noreply.github.com>
2024-08-13 15:18:38 +0530

    Update streamlit_app.py
    adding the response for the selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
     #st.write(ingredients_string)
 
