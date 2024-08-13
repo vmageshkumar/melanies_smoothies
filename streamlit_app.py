fd42e94d551237f00ccae10886de0d11da183d8d
Mahesh Kumar <69344132+vmageshkumar@users.noreply.github.com>
2024-08-13 15:46:17 +0530

    Update streamlit_app.py
    added reso

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write('The name on your Smoothie will be:', name_on_order )
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
@@ -32,6 +32,9 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(each_fruit + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
     #st.write(ingredients_string)
