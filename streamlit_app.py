ebb2b5c39edd1cd46bc0676399b443ae1eeded9b
Mahesh Kumar <69344132+vmageshkumar@users.noreply.github.com>
2024-08-13 15:48:08 +0530

    Update streamlit_app.py
    removing stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+
 ingredients_list = st.multiselect('Choos up to 5 Ingrediants:', 
                                   my_dataframe, 
                                   max_selections = 5
